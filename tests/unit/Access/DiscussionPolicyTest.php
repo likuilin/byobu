1d16dd6808de7674975d585fe4967de272e1b625
Daniel Klabbers <daniel@klabbers.email>
2018-10-04 13:58:40 +0200

    first

---
+++ tests/unit/Access/DiscussionPolicyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Flagrow\Byobu\Tests\Access;
+
+use Flagrow\Steamroller\TestCase;
+
+class DiscussionPolicyTest extends TestCase
+{
+    public function guest_cannot_view_private_discussion()
+    {
+
+    }
+}
